c8b7b25f56987a7574abf1c6b0ceb3f030d995d8
jacquelyn bloomfield <jbloomfield16@lapu.edu>
2024-03-08 12:24:48 +0000

    header and footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,24 @@
-export default function Footer() {
+import React from 'react';
+import './Footer.css';
 
+export default function Footer () {
     return (
-        <h3>Here coud be footer</h3>
-    )
+        <footer>
+            <div className="footer-container">
+                <p>&copy; 2024 Gang of Four</p>
+            
+                <p>Follow us on
+                    <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
+                        <span className="icon icon-twitter"></span>
+                    </a>
+                    <a href="https://www.facebook.com/S20.officialPage" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+                        <span className="icon icon-facebook"></span>
+                    </a>
+                    <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+                        <span className="icon icon-instagram"></span>
+                    </a>
+                </p>
+            </div>
+        </footer>
+    );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,14 @@
+import { Link } from 'react-router-dom';
+import './Header.css';
+
 export default function Header() {
 
     return (
-        <h3>Here coud be header</h3>
-    )
+        <div className="header-container">
+            <h1>Authors of Our Time</h1>
+            <div className="header-nav">
+                <Link to={'/'} > <a href="#home">Home</a> </Link>
+            </div>
+        </div>
+    );
 }
